fb3bcb6d1b3128243d38108df654a56592b8f384
Andrés Martínez <amartinez@stackbuilders.com>
2023-03-06 16:32:52 -0500

    adding user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :null_session
+    
+    rescue_from ActiveRecord::RecordNotFound do |e|
+        data = { message: e.message }
+        render json: data, status: :not_found
+    end
 end
